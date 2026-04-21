88a3902877a98577bf81b39ed336c43bc3ab0bcc
Djem Andreif Reyes <djemandreif@gmail.com>
2026-04-21 00:51:09 +0000

    Updated resto app

--- src/RestoApp.java
+++ src/RestoApp.java
@@ -1,17 +1,25 @@
 public class RestoApp {
     public static void main(String[] args) {
-        
-        System.out.println("--- MAIN MENU ---")
+        // Create Leaves for the meal
+        MenuItem burger = new MenuItem("Classic Burger", 250.00);
+        MenuItem fries = new MenuItem("Large Fries", 85.00);
+        MenuItem rootBeer = new MenuItem("Root Beer", 60.00);
 
+        // Create Sub-Composite and add the meal leaves
         MenuCategory barkadaMeal = new MenuCategory("Barkada Solo Meal");
+        barkadaMeal.add(burger);
+        barkadaMeal.add(fries);
+        barkadaMeal.add(rootBeer);
 
-        barkadaMeal.add(new MenuItem("Classic Burger", 250.00));
-        barkadaMeal.add(new MenuItem("Large Fries", 85.00));
-        barkadaMeal.add(new MenuItem("Root Beer", 60.00));
-        barkadaMeal.add(new MenuItem("Vanilla Sundae", 45.00));
+        // Create standalone Leaf
+        MenuItem sundae = new MenuItem("Vanilla Sundae", 45.00);
 
+        // Create Top-Composite and add the sub-composite and the standalone leaf
+        MenuCategory mainMenu = new MenuCategory("Main Menu");
         mainMenu.add(barkadaMeal);
+        mainMenu.add(sundae);
 
+        // Test: Print the entire structure and the total price
         mainMenu.print();
         
         System.out.println("\n==============================");
